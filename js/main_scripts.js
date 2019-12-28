e9055f45ec77dc3dc51ee9c9d013c64e3a1827e4
Syed Baji Shaik <syedbs@langoor.com>
2019-12-28 09:59:13 +0530

    complete site

--- js/main_scripts.js
+++ js/main_scripts.js
@@ -3,6 +3,7 @@ $(document).ready(function(){
         loop:true,
         margin:10,
         nav:true,
+        dots: false,
         responsive:{
             0:{
                 items:1
@@ -15,4 +16,31 @@ $(document).ready(function(){
             }
         }
     })
+    $('.testimonial-carousel').owlCarousel({
+        loop:true,
+        center: true,
+        margin:10,
+        nav:true,
+        singleItem:true,
+        dots: false,
+        onDragged: callback,
+        onTranslated: callback,
+        responsive:{
+            0:{
+                items:1
+            },
+            600:{
+                items:1
+            },
+            1000:{
+                items:3
+            }
+        }
+    });
+    function callback(event) {
+        $('.testimonial-carousel .owl-item').removeClass('prev next');
+        $('.testimonial-carousel .owl-item.active.center').prev().addClass('prev');
+        $('.testimonial-carousel .owl-item.active.center').next().addClass('next');
+    }
+    callback();
 });
