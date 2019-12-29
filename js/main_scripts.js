28d46f9aebec1c3a04b71dfc230729f6b0beb55f
Syed Baji Shaik <syedbs@langoor.com>
2019-12-29 09:35:11 +0530

    with changes

--- js/main_scripts.js
+++ js/main_scripts.js
@@ -1,4 +1,24 @@
 $(document).ready(function(){
+    $('.main-slider-carousel').owlCarousel({
+        loop: true,
+        margin: 0,
+        nav: false,
+        dots: false,
+        autoplay: true,
+        autoplayTimeout: 5000,
+        autoplayHoverPause: false,
+        responsive:{
+            0:{
+                items:1
+            },
+            600:{
+                items:1
+            },
+            1000:{
+                items:1
+            }
+        }
+    });
     $('.profile-carousel').owlCarousel({
         loop:true,
         margin:10,
